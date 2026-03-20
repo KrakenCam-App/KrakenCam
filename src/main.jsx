4351b2ed16a8956f25b5a449574770ef7247a0cb
JKhouvongsavanh <47010811+JKhouvongsavanh@users.noreply.github.com>
2026-03-20 11:06:25 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './jobsite-reporter.jsx'
+import { AuthProvider } from './components/AuthProvider.jsx'
+import AppRouter from './AppRouter.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  </React.StrictMode>
+)
